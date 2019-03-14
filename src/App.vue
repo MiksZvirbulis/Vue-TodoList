bfc042aae38b499369d1dee510a8b3f503b9c178
Miks Zvirbulis <mikszvirbulis@yahoo.com>
2019-03-14 16:57:19 +0000

    Added some routing

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,17 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Header from './components/layout/Header'
-import Todos from './components/Todos'
-import AddTodo from './components/AddTodo'
-import axios from 'axios'
-
+import Header from './components/layout/Header';
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    Header,
-    Todos,
-    AddTodo
-  },
-  data() {
-    return {
-      todos: []
-    }
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id )
-    },
-    addTodo(newTodo) {
-      this.todos = [ ...this.todos, newTodo ]
-    }
-  },
-  created() {
-      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
-      .then(res => this.todos = res.data)
-      .catch(err => console.log(err))
-    }
+    Header
+  }
 }
 </script>
 
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="about">
+        <h1>About</h1>
+        <p>About us page has no content.</p>
+    </div>
+</template>
